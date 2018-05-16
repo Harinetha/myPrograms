d598fa8340ae14b2f64f738f7a3debffc7f6f98b
Harinetha <39326594+Harinetha@users.noreply.github.com>
2018-05-16 16:37:52 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -6,6 +6,6 @@ int main()
     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
     printf("Vowel");
     else 
-    printf("Not a Vowel");
+    printf("Consonant");
     return 0;
 }
