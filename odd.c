efd3f107dec4fd09104086ed79e35fff55feaaea
Harinetha <39326594+Harinetha@users.noreply.github.com>
2018-05-16 16:56:54 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    if(n%2==0)
+    int num;
+    scanf("%d",&num);
+    if(num%2==0)
     printf("Even");
     else 
     printf("Odd");
