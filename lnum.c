1dd0a8d7f2011414705eb401923290008c0e2e7d
Harinetha <39326594+Harinetha@users.noreply.github.com>
2018-05-21 17:14:10 +0530

    Rename maxnum.c to lnum.c

---
+++ lnum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+    int l,n,max,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
