3e261769abbe73c8d781d6f276383bfa4709e1a4
Harinetha <39326594+Harinetha@users.noreply.github.com>
2018-07-17 13:17:51 +0530

    Create equalIgnoreCase.c

---
+++ equalIgnoreCase.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int l1,l2,c=0,i;
+    char str1[10],str2[10];
+    scanf("%s",str1);
+    scanf("%s",str2);
+    l1=strlen(str1);
+    l2=strlen(str2);
+    if(l1!=l2)
+    printf("no");
+    else{
+        for(i=0;i<l1;i++){
+            if(str1[i]>=65 && str1[i]<=90)
+                str1[i]=str1[i]+32;
+            if(str2[i]>=65 && str2[i]<=90)
+                str2[i]=str2[i]+32;
+            if(str1[i]==str2[i])
+                c++;
+        }
+    }
+    if(c==l1)
+        printf("yes");
+    else
+        printf("no");
+    return 0;
+}
