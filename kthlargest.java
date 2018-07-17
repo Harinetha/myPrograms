d6ae43f6d0ae379717d89b7be26bdbfb8cc63e29
Harinetha <39326594+Harinetha@users.noreply.github.com>
2018-07-17 17:14:13 +0530

    Create kthlargest.java

---
+++ kthlargest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+public class Main{
+    public static void main(String aa[]){
+       
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int k=s.nextInt();
+    ArrayList<Integer>ar=new ArrayList<Integer>();
+    for(int i=0;i<n;i++){
+        int c=s.nextInt();
+        ar.add(c);
+    }
+   ArrayList<Integer>v=new ArrayList<Integer>(ar);
+   Collections.sort(v);
+   Collections.sort(v,Collections.reverseOrder());
+   System.out.println(v.get(k-1));
+}
+}
